1ef8fe8c36d83dd51d7465ab014df063ab2d5612
galaxnet <55704207+galaxnet@users.noreply.github.com>
2022-02-24 15:09:02 -0500

    Fixed ::APPLYLUT filter order
    Fixed filter order so lossiest filters at the end, but testing hasn't shown much difference

--- ffmenu.bat
+++ ffmenu.bat
@@ -213,7 +213,7 @@ EXIT /B 0
 :APPLYLUT
 REM Change "medium" to slow, slower, or veryslow for higher quality video
 SET /P LUT=Type the name of the LUT the press ENTER (no extension):
-ffmpeg -i %* -vf lut3d=file="lut/%LUT%.CUBE",hqdn3d=6,unsharp=5:5:0.5:7:7:0.5,scale=1920x1080:flags=lanczos -c:a copy -c:v libx264 -crf 22 -preset medium out.mp4
+ffmpeg -i %* -vf hqdn3d=6,unsharp=5:5:0.5:7:7:0.5,scale=1920x1080:flags=lanczos,lut3d=file="lut/%LUT%.CUBE" -c:a copy -c:v libx264 -crf 22 -preset medium "%~n1_%LUT%.mp4"
 EXIT /B 0
 
 :TESTLUTPHOTO
